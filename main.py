b744309d6a06e42074fdb82a8ad67774a737e9bb
Chahrazad650 <Dahmanichahrazad4@gmail.com>
2026-01-28 18:17:02 +0100

    Exo2

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ def getsoup(s: str) -> BeautifulSoup:
     return BeautifulSoup(rq.get(s).text, 'html.parser')
 
 soup = getsoup("https://www.millesima.fr/")
+
+def nimportequoi() :
+    links = soup.find_all('a')
+    for link in links:
+        print(link.get('href'))         
