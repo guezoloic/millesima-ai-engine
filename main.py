993246f485d2f4fedeec3afb3f80fbdeffa40fae
Loïc GUEZO <loicguezo@gmail.com>
2026-01-28 18:03:38 +0100

    ajout(main.py): ajout fonction soup

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from bs4 import BeautifulSoup
 import requests as rq
+
+def getsoup(s: str) -> BeautifulSoup:
+    return BeautifulSoup(rq.get(s).text, 'html.parser')
+
+soup = getsoup("https://www.millesima.fr/")
