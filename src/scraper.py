f5d5703e4985c262edb2595069264c9bad7d99be
Loïc GUEZO <loicguezo@gmail.com>
2026-03-27 21:47:06 +0100

    fix(scraper): recherche _getproduitslist actualisé
    Suite à une refont de l'UI et du backend, la structure de données JSON envoyé par la page web a été simplifié.
    
    Ancienne structure:
    
    - `"props"->"pageProps"->"initialReduxState"->"categ"->"content->"produits"`
    
    Nouvelle structure:
    
    - `"props"->"pageProps"->"produits"`

--- src/scraper.py
+++ src/scraper.py
@@ -377,12 +377,17 @@ class Scraper:
         try:
             data: dict[str, object] = self.getjsondata(subdir).getdata()
 
-            for element in ["initialReduxState", "categ", "content"]:
-                data = cast(dict[str, object], data.get(element))
+            # Changement dans la maniere du site stocke ses données.
+            #  
+            # for element in ["initialReduxState", "categ", "content"]:
+            #     data = cast(dict[str, object], data.get(element))
+            #     print(data)
 
             products: list[dict[str, Any]] = cast(
                 list[dict[str, Any]], data.get("products")
             )
+
+            print(products)
 
             return products
 
